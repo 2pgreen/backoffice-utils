9aada5f375544cd22a9829ac4b2bcfa8795a1b8f
Rung <rungwiroon@gmail.com>
2021-08-26 01:40:00 +0700

    Define months

---
+++ src/date.ts
@@ -0,0 +1,14 @@
+export const Months = [
+    { No: 1, Name: "JAN" },
+    { No: 2, Name: "FEB" },
+    { No: 3, Name: "MAR" },
+    { No: 4, Name: "APR" },
+    { No: 5, Name: "MAY" },
+    { No: 6, Name: "JUN" },
+    { No: 7, Name: "JUL" },
+    { No: 8, Name: "AUG" },
+    { No: 9, Name: "SEP" },
+    { No: 10, Name: "OCT" },
+    { No: 11, Name: "NOV" },
+    { No: 12, Name: "DEC" },
+]
